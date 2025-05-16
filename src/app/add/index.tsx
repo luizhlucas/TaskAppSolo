0ca43d89a219b63b8ec74bf820691a64d59b894c
luizhlucas <luizhlucaspereira@gmail.com>
2025-05-16 18:45:34 -0300

    Remove

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { View, Text } from "react-native"
+import { View, Alert } from "react-native"
 import { router } from "expo-router"
 
 
@@ -12,21 +12,31 @@ import { Input } from "@/components/input"
 import { taskStorage } from "@/storage/task-Storage"
 
 
-export default function Index() {
+export default function Add() {
     const [name, setName] = useState("")
 
     async function handlepress(){
+        if (!name.trim()) {
+            return Alert.alert("No Task","enter some task")
+        }
+
         await taskStorage.save({
             id: new Date().getTime().toString(),
             name,
         })
 
+        Alert.alert("success", "New task added", [
+                {
+                    text: "Ok",
+                    onPress: () => router.back()
+                }   
+        ])
     }
 
     return(
         <View style={styles.container}>
             <View style={styles.header}>
-                <Icon iconName="arrow-back" iconcolor={colors.green[100]} onPress={() => router.back()}/>
+                <Icon iconName="arrow-back" iconColor={colors.green[100]} onPress={() => router.back()}/>
             </View>
             <View style={styles.content}>
                 <Input onChangeText={setName}/>
